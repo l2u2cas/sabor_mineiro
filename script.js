0255340365af75e12b59d7ceab5fd05eb494afa0
LUCAS LIMA <lucaslima1912@live.com>
2023-10-18 19:24:56 -0300

    sabor

---
+++ script.js
@@ -0,0 +1,27 @@
+const body = document.querySelector("body")
+
+
+
+body.innerHTML = `
+<h1 id="titulo"> Laticínio Sabor mineiro <h1/>
+<section>
+    <ul>
+        <li>Mussarela : R$ 39,00 kg</li>
+        <li>Queijo minas: R$ 15,00 kg</li>
+        <li>Queijo padrão:R$ 65,00 kg</li>
+        <li>Bolinha queijo:R$ 35,00 kg</li>
+    </ul>
+    <img src="" alt="">
+</section>
+    
+
+
+
+
+
+
+
+
+
+
+`
