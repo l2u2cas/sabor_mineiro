a2de2e73bac15dc457fd7a4e64ac4125694ddd48
LUCAS LIMA <lucaslima1912@live.com>
2023-10-18 19:48:52 -0300

    Queijo minas

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-const body = document.querySelector("body")
+const titulo = document.createElement("h1");
+const body = document.querySelector("body");
+const produto = document.createElement("section");
 
+titulo.id = "titulo";
+titulo.innerText = "Laticinios Sabor mineiro";
 
+body.appendChild(titulo);
 
-body.innerHTML = `
-<h1 id="titulo"> Laticínio Sabor mineiro <h1/>
-<section>
-    <ul>
-        <li>Mussarela : R$ 39,00 kg</li>
-        <li>Queijo minas: R$ 15,00 kg</li>
-        <li>Queijo padrão:R$ 65,00 kg</li>
-        <li>Bolinha queijo:R$ 35,00 kg</li>
-    </ul>
-    <img src="" alt="">
-</section>
-    
-
-
-
-
-
-
-
-
-
-
+produto.innerHTML = `
+  <section>
+    <h2>Queijo minas</h2>
+    <img src="https://ibassets.com.br/ib.item.image.big/b-fba6777efecd4b4398b68a6c42ae3b80.png" alt="queijo minas">
+    <strong><p>O queijo minas frescal encontra-se entre os queijos mais consumidos no Brasil. Apresenta massa crua, coloração esbranquiçada, consistência mole e textura fechada. O preço varia em torno de: </p><strong/>
+    <p> R$ 20.90 kg</p>
+  </section>
 `
+
+
+body.appendChild(produto)
+
+
+
+
+
+
+
+
+
